108bacb1e685de2de277b003798bbf5cdacde30a
mc.raul86@gmail.com <mc.raul86@gmail.com>
2025-03-10 00:21:58 -0400

    Añadido datos de vibracion de motor

--- tests/test2.py
+++ tests/test2.py
@@ -9,27 +9,42 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 # Codifica la imagen ISO 10816
 encoded_image = base64.b64encode(open('img/image.png', 'rb').read()).decode()
 
+def clean_data(df):
+    # Limpiar nombres de columnas eliminando espacios adicionales
+    df.columns = df.columns.str.strip()
+    # TypeError: unsupported operand type(s) for +: 'DatetimeArray' and 'str'
+    df['Timestamp'] = pd.to_timedelta(df['Desviación de Tiempo'], unit='ms')
+    df['Timestamp'] = pd.to_datetime(df['Fecha'].astype(str) + ' ' + df['Hora'].astype(str),errors="coerce") + df['Timestamp']
+    # Remove columns Fecha, Hora, and Desviación de Tiempo
+    df = df.drop(columns=['Fecha', 'Hora', 'Desviación de Tiempo','Delta(ms)'])
+    # Eliminar filas con valores nulos en 'Valor'
+    df = df.dropna(subset=['Valor'])
+    return df
+
+import pandas as pd
+df = pd.read_excel('data/Valores 28-10-22 LMO motor TDB.xlsx', sheet_name='Valores Punto1 FO 28-10-22', header=6, usecols='A:E')
+df_clean = clean_data(df.copy())
+df_clean.head()
+#df
 app.layout = dbc.Container(fluid=True, style={'fontFamily': 'Arial Narrow', 'padding': '20px'}, children=[
     dbc.Tabs([
         dbc.Tab(label='Vibración', children=[
             dbc.Card([
                 dbc.CardBody([
-                    dbc.ButtonGroup([
-                        dbc.Button('START', color='success', style={'width': '200px'}),
-                        dbc.Button('STOP', color='danger', style={'width': '200px'})
-                    ], className='mb-3'),
+                    
 
                     html.H3('Registro de Vibración GRUPO 1', className='text-center text-danger'),
 
                     dbc.ButtonGroup([
-                        dbc.Button('Grupo 1', color='primary'),
-                        dbc.Button('Grupo 2', color='secondary'),
-                        dbc.Button('ISO 10816', color='info')
+                        dbc.Button('Eje X', color='primary'),
+                        dbc.Button('Eje Y', color='secondary'),
+                        dbc.Button('Eje Z', color='info')
                     ], className='mb-3'),
 
                     dbc.Row([
                         dbc.Col([
-                            dbc.Button('EJE X', color='info', className='rounded-circle', style={'width': '80px', 'height': '80px'}),
+                            #dbc.Button('EJE X', color='info', className='rounded-circle', style={'width': '80px', 'height': '80px'})
+                            
                             html.Div('AZUL EJE "X"', className='mt-2 text-info text-center')
                         ], width=2),
 
@@ -38,7 +53,8 @@ app.layout = dbc.Container(fluid=True, style={'fontFamily': 'Arial Narrow', 'pad
                                 id='vibration-graph',
                                 figure={
                                     'data': [
-                                        go.Scatter(x=[], y=[], mode='lines', name='Vibración', line={'color': '#1f77b4'})
+                                        go.Scatter(x=df_clean['Timestamp'], y=df_clean['Valor'], mode='lines', name='Valor Acelerometro')
+                                        #go.Scatter(x=[], y=[], mode='lines', name='Vibración', line={'color': '#1f77b4'})
                                     ],
                                     'layout': go.Layout(
                                         title="",
